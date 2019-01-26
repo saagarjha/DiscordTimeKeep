f9e92e6ca70a8172672dd539a69a2415836d2779
TropicMango <charliean19@gmail.com>
2019-01-26 00:47:11 -0800

    data backup, about to change reaping time add

---
+++ SecretFile.py
@@ -0,0 +1,2 @@
+def get_token():
+    return 'NTM4MDc4MDYxNjgyMjI5MjU4.Dy0r1w.gN0KF-5D7NJnH8k63N5QIGtB7-g'
--- TimeKeep.py
+++ TimeKeep.py
@@ -13,7 +13,7 @@ bot.remove_command("help")
 
 stored_time = 0
 latest_clear = 0
-cooldown = 43200
+CD = 43200
 
 
 async def start_timer():
@@ -38,15 +38,21 @@ async def on_ready():  # when ready it prints the username, id, and starts the s
     print(bot.user.id)
     print('------')
     global latest_clear
-    latest_clear = time.time()
+    latest_clear = float(get_latest_time())
     await start_timer()
+
+
+def get_latest_time():
+    with open("./data/playerData.txt", "r") as f:
+        content = f.readlines()
+    return content[0]
 
 
 @bot.command(pass_context=True)
 async def reap(ctx):
-    global latest_clear
     author_id = ctx.message.author.id
     contain = False
+    update = True
     added_time = int(time.time() - latest_clear)
 
     # database storage
@@ -66,22 +72,41 @@ async def reap(ctx):
                                             int(flowed_time % 60)))  # seconds
                     contain = True
                     f.write(line)
+                    update = False
                 else:
-                    await bot.say('<@!{}> has added {} seconds to their total'.format(author_id, added_time))
+                    await bot.say('<@!{}> has added {} to their total'.format(author_id, seconds_format(added_time)))
                     s_line[2] = int(s_line[2]) + added_time
-                    s_line[3] = time.time() + cooldown
-                    f.write('|'.join(str(x) for x in s_line))
+                    s_line[3] = time.time() + CD
+                    f.write('|'.join(str(x) for x in s_line) + '\n')
                     contain = True
-                    latest_clear = time.time()
             else:
                 f.write(line)
         if not contain:
-            await bot.say('<@!{}> has added {} seconds to their total'.format(author_id, added_time))
-            init = '{}|{}|{}|{}'.format(author_id, ctx.message.author, added_time, time.time() + cooldown)
+            await bot.say('<@!{}> has added {} to their total'.format(author_id, seconds_format(added_time)))
+            init = '{}|{}|{}|{}'.format(author_id, ctx.message.author, added_time, time.time() + CD)
             print(init)
             f.write(str(init) + '\n')
-            latest_clear = time.time()
             await update_time()
+    if update:
+        set_time()
+
+
+def set_time():
+    global latest_clear
+    latest_clear = time.time()
+    with open("./data/playerData.txt", "r") as f:
+        content = f.readlines()
+    with open("./data/playerData.txt", "w") as f:
+        print('added')
+        first_line = True
+        for line in content:
+            if first_line:
+                print(time.time())
+                f.write(str(time.time()) + '\n')
+                first_line = False
+
+            else:
+                f.write(line)
 
 
 @bot.command(pass_context=True)
@@ -91,11 +116,16 @@ async def me(ctx):
     for line in content:
         if line.startswith(ctx.message.author.id):
             seconds = float(line.split('|')[2])
-            await bot.say('<@!{}> have stored {} seconds\nor {} hours {} minutes and {} seconds'.format(
-                ctx.message.author.id, seconds,
-                int(seconds / 3600),  # hours
-                int((seconds % 3600) / 60),  # minutes
-                int(seconds % 60)))  # seconds
+            if float(line.split('|')[3]) - time.time() > 0:
+                next_reap = seconds_format(float(line.split('|')[3]) - time.time())
+            else:
+                next_reap = 'Your Next Reap is Up'
+            await bot.say('<@!{}> have stored {} seconds\n'
+                          'or {}\n Next Reap: {}'
+                          ''.format(
+                            ctx.message.author.id, seconds,
+                            seconds_format(seconds),
+                            next_reap))
             return
     await bot.say('<@!{}> have stored 0 seconds'.format(ctx.message.author.id))
 
@@ -126,6 +156,20 @@ async def help():
                         "t!me: see how much time you reaped\n" \
                         "t!leaderboard: WIP\n"
     await bot.say(embed=embed)
+
+
+def seconds_format(seconds):
+    return '{} Hours {} Minutes {} Seconds'.format(
+        int(seconds / 3600),  # hours
+        int((seconds % 3600) / 60),  # minutes
+        int(seconds % 60))  # seconds
+
+
+@bot.command(pass_context=True)
+async def leaderboard(ctx):
+    with open("./data/playerData.txt", "r") as f:
+        content = f.readlines()
+    print(content[0])
 
 
 bot.run(SecretFile.get_token())  # are you happy now Soap????
